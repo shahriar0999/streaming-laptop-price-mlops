f1e5caf34c9088f9e4d047662e4577bb9d556be9
shahriar0999 <shahriar0999@proton.me>
2026-04-29 17:51:40 +0600

    changes

--- notebooks/data_cleaning.py
+++ notebooks/data_cleaning.py
@@ -1,10 +1,30 @@
-# %% Databricks notebook source
+# Databricks notebook source
 %pip install -e ..
 %restart_python
 
 
 # COMMAND ----------
-
+!pip install yaml
 
 print("hello")
+# COMMAND ----------
+from pathlib import Path
+import sys
+sys.path.append(str(Path.cwd().parent / 'src'))
+
+
+# COMMAND ----------
+import pandas as pd
+import yaml
+from loguru import logger
+from pyspark.sql import SparkSession
+
+from marvel_characters.config import ProjectConfig
+from marvel_characters.data_processor import DataProcessor
+
+config = ProjectConfig.from_yaml(config_path="../project_config_marvel.yml", env="dev")
+
+logger.info("Configuration loaded:")
+logger.info(yaml.dump(config, default_flow_style=False))
+
 # COMMAND ----------
